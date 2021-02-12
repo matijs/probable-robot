3dc333d4cc9c54673101a3d33b4c36c857dced0d
Matijs Brinkhuis <matijs@gmail.com>
2021-02-12 20:13:23 +0100

    WIP

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  purge: {
+    preserveHtmlElements: false,
+    content: ['public//2021/02/tailwind-draft/index.html'],
+    options: {
+      keyframes: true,
+    },
+  },
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+    screens: {},
+    fontFamily: {
+      serif: ['Palatino', 'serif'],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
